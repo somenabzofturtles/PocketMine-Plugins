f153ac6f358c157eb488e8c882d7fe77812d5b56
Gamecrafter <pro_victor@ymail.com>
2015-08-31 22:29:23 -0700

    PlanB: Initial commit

---
+++ PlanB/src/planb/PlanB.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace planb;
+
+use planb\command\PlanBCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class PlanB extends PluginBase{
+    /** @var Config */
+    public $backups;
+    public function onEnable(){
+        $this->saveFiles();
+        $this->registerAll();
+    }
+    private function saveFiles(){
+    	if(!is_dir($this->getDataFolder())) mkdir($this->getDataFolder());
+        if(file_exists($this->getDataFolder()."config.yml")){
+            if($this->getConfig()->get("version") !== $this->getDescription()->getVersion() or !$this->getConfig()->exists("version")){
+		$this->getServer()->getLogger()->warning("An invalid configuration file for ".$this->getDescription()->getName()." was detected.");
+		if($this->getConfig()->getNested("plugin.autoUpdate") === true){
+		    $this->saveResource("config.yml", true);
+                    $this->getServer()->getLogger()->warning("Successfully updated the configuration file for ".$this->getDescription()->getName()." to v".$this->getDescription()->getVersion().".");
+		}
+	    }  
+        }
+        else{
+            $this->saveDefaultConfig();
+        }
+        $this->backups = new Config($this->getDataFolder()."backups.txt", Config::ENUM);
+    }
+    private function registerAll(){
+        $this->getServer()->getCommandMap()->register("planb", new PlanBCommand($this));
+    }
+    /**
+     * @param type $player
+     * @return bool
+     */
+    public function isBackupPlayer($player){
+        return $this->backups->exists(strtolower($player), true);
+    }
+    /**
+     * @param string $player
+     */
+    public function addBackup($player){
+        $this->backups->set(strtolower($player));
+        $this->backups->save();
+    }
+    /**
+     * @param string $player
+     */
+    public function removeBackup($player){
+        $this->backups->remove(strtolower($player));
+        $this->backups->save();
+    }
+    /**
+     * @param CommandSender $sender
+     */
+    public function sendBackups(CommandSender $sender){
+        $sender->sendMessage(TextFormat::YELLOW."List of all backup players:");
+        $sender->sendMessage(file_get_contents($this->getDataFolder()."backups.txt"));
+    }
+    public function restoreOps(){
+        foreach($this->getServer()->getOnlinePlayers() as $player){
+            if(!$this->isBackupPlayer($player->getName()) and $player->isOp()){
+                $player->setOp(false);
+                $player->close("", $this->getConfig()->getNested("backup.kickReason"));
+                if($this->getConfig()->getNested("backup.notifyAll") === true){
+                    $this->getServer()->broadcastMessage(TextFormat::YELLOW."Deopped and kicked potential hacker: ".$player->getName());
+                }
+            }
+            if($this->isBackupPlayer($player->getName()) and !$player->isOp()){
+                $player->setOp(true);
+                $player->sendMessage(TextFormat::YELLOW."Your OP status is being restored...");
+            }
+        }
+    }
+}
---
+++ PlanB/src/planb/command/PlanBCommand.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace planb\command;
+
+use planb\PlanB;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\utils\TextFormat;
+
+class PlanBCommand extends Command implements PluginIdentifiableCommand{
+    /** @var PlanB */
+    private $plugin;
+    public function __construct(PlanB $plugin){
+        parent::__construct(
+            "planb", 
+            "Shows all the sub-commands for PlanB", 
+            "/planb <sub-command> [parameters]", 
+            array("pb")
+        );
+        $this->setPermission("planb.command.planb");
+        $this->plugin = $plugin;
+    }
+    /** @return \planb\PlanB */
+    public function getPlugin(){
+        return $this->plugin;
+    }
+    /**
+     * @param CommandSender $sender
+     */
+    private function sendCommandHelp(CommandSender $sender){
+        $sender->sendMessage("PlanB commands:");
+        $sender->sendMessage("/planb addbackup: Adds a player to backups.txt");
+        $sender->sendMessage("/planb delbackup: Removes a player from backups.txt");
+        $sender->sendMessage("/planb help: Shows all the sub-commands for PlanB");
+        $sender->sendMessage("/planb list: Lists all backup players");
+        $sender->sendMessage("/planb restore: Restores OP status of all online players listed in backup.txt");
+    }
+    public function execute(CommandSender $sender, $label, array $args){
+        if(isset($args[0])){
+            if(strtolower($args[0]) === "addbackup" or strtolower($args[0]) === "ab"){
+                if(isset($args[1])){
+                    if($sender instanceof ConsoleCommandSender){
+                        if($this->getPlugin()->isBackupPlayer($args[1])){
+                            $sender->sendMessage(TextFormat::RED.$args[1]." already exists in backups.txt.");
+                        }
+                        else{
+                            $this->getPlugin()->addBackup($args[1]);
+                            $sender->sendMessage(TextFormat::GREEN."Added ".$args[1]." to backups.txt.");
+                        }
+                    }
+                    else{
+                        $sender->sendMessage(TextFormat::RED."Please run this command on the console.");
+                    }
+                }
+                else{
+                    $sender->sendMessage(TextFormat::RED."Please specify a valid player."); 
+                }
+                return true;
+            }
+            if(strtolower($args[0]) === "delbackup" or strtolower($args[0]) === "db"){
+                if(isset($args[1])){
+                    if($sender instanceof ConsoleCommandSender){
+                        if($this->getPlugin()->isBackupPlayer($args[1])){
+                            $this->getPlugin()->removeBackup($args[1]);
+                            $sender->sendMessage(TextFormat::GREEN."Removed ".$args[1]." from backups.txt.");
+                        }
+                        else{
+                            $sender->sendMessage(TextFormat::RED.$args[1]." does not exist in backups.txt.");
+                        }
+                    }
+                    else{
+                        $sender->sendMessage(TextFormat::RED."Please run this command on the console.");
+                    }
+                }
+                else{
+                    $sender->sendMessage(TextFormat::RED."Please specify a valid player.");
+                }
+                return true;
+            }
+            if(strtolower($args[0]) === "help"){
+                $this->sendCommandHelp($sender);
+                return true;
+            }
+            if(strtolower($args[0]) === "list"){
+                $this->getPlugin()->sendBackups($sender);
+                return true;
+            }
+            if(strtolower($args[0]) === "restore"){
+                if($this->getPlugin()->isBackupPlayer($sender->getName()) or $sender instanceof ConsoleCommandSender){
+                    $this->getPlugin()->restoreOps();
+                    $sender->sendMessage(TextFormat::YELLOW."Restoring the statuses of OPs...");
+                }
+                else{
+                    $sender->sendMessage(TextFormat::RED."You do not not have permissions to restore OPs.");
+                }
+                return true;
+            }
+            else{
+                $sender->sendMessage("Usage: ".$this->getUsage());
+            }
+        }
+        else{
+            $this->sendCommandHelp($sender);
+        }
+    }
+}
