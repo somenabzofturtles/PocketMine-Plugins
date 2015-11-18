7e54c1c90c01559cbe10b171c8414c257d669d0b
Gamecrafter <pro_victor@ymail.com>
2015-11-17 21:41:53 -0800

    QueryFacade: Some fixes

--- QueryFacade/src/queryfacade/network/FakePlugin.php
+++ QueryFacade/src/queryfacade/network/FakePlugin.php
@@ -17,28 +17,61 @@ class FakePlugin implements Plugin{
     public function __construct($name, $version = "1.0.0"){
         $this->description = new PluginDescription(["name" => (string) $name, "main" => "", "version" => (string) $version, "api" => "1.0.0"]);
     }
+    public function onLoad(){
+    }
+    public function onEnable(){
+    }
+    public function isEnabled(){
+    }
+    public function onDisable(){
+    }
+    public function isDisabled(){
+    }
+    public function getDataFolder(){
+    }
     /**
      * @return PluginDescription
      */
     public function getDescription(){
         return $this->description;
     }
-    public function onLoad();
-    public function onEnable();
-    public function isEnabled();
-    public function onDisable();
-    public function isDisabled();
-    public function getDataFolder();
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args);
-    public function getResource($filename);
-    public function saveResource($filename, $replace = false);
-    public function getResources();
-    public function getConfig();
-    public function saveConfig();
-    public function saveDefaultConfig();
-    public function reloadConfig();
-    public function getServer();
-    public function getName();
-    public function getLogger();
-    public function getPluginLoader();
+    /**
+     * @param CommandSender $sender
+     * @param Command $command
+     * @param string $label
+     * @param string[] $args
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        return false;
+    }
+    /**
+     * @param string $filename
+     */
+    public function getResource($filename){
+    }
+    /**
+     * @param string $filename
+     * @param bool $replace
+     */
+    public function saveResource($filename, $replace = false){
+    }
+    public function getResources(){
+    }
+    public function getConfig(){
+    }
+    public function saveConfig(){
+    }
+    public function saveDefaultConfig(){
+    }
+    public function reloadConfig(){
+    }
+    public function getServer(){
+    }
+    public function getName(){
+    }
+    public function getLogger(){
+    }
+    public function getPluginLoader(){
+    }
 }
